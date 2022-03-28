59c958752ca34a5351c74601cf52eaed56281bc1
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-28 09:13:23 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,26 @@
+#Duck typing 
+class Duck:
+  def walk(self):
+    print("duck walk")
+    
+class Horse:
+  def walk(self):
+    print('horse walk')
+
+class Cat:
+  def talk(self):
+    print("cat talk:")
+  
+def myFunction(obj):
+  obj.walk()
+
+
+d = Duck()  #duck object created
+myFunction(d) 
+
+h = Horse() #horse object created
+myFunction(h)
+
+c = Cat()  #this will give error 
+myfunction(c) 
+
